50ca4a60ed2cc46e79d48da109b224bbd7d38fb1
Tomilola Oluwafemi <tee.o2809@gmail.com>
2025-03-27 16:10:34 +0100

    Feat: Update tweet workflow schedule and integrate Airtable API keys; refactor main function to utilize SocialMediaManager

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 """Main module for AI-agent."""
 
-from agents.daily_post import DailyPostAgent
+from agents.social_media.manager import SocialMediaManager
 
 
 MAX_RETRIES = 3
 
+
 def main():
     """Main function for AI-agent."""
     print("Starting AI-agent...")
-    count = 0
+    smm_agent = SocialMediaManager()
+    print(smm_agent.run())
 
-    print("Generating daily post...")
-    daily_post_agent = DailyPostAgent()
-    get_posts = daily_post_agent.generate_post()
-    # Checks if the post is too long and generates a new one when under 3 retries
-    while len(get_posts) > 280:
-        count += 1
-        if count > MAX_RETRIES:
-            raise ValueError("Post is too long, cannot post on twitter.")
-        get_posts = daily_post_agent.generate_post()
-        print(f"Post is too long, generating new post: {get_posts}, retrying... {count}/{MAX_RETRIES}")
-    # Makes a post on twitter
-    print(f"Post generated: {get_posts}")
-    print("Posting on Twitter...")
-    daily_post_agent.post_on_twitter(get_posts)
-    print("Post successfully posted on Twitter.")
-    print("AI-agent completed.")
 
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,13 +1,9 @@
 """Tests for AI-agent."""
 
 import os
-import requests
 
 from dotenv import load_dotenv
-from requests_oauthlib import OAuth1
 from utils.openai import OpenAIClient
-
-from agents.social_media.content_creator import test as test_content_creator
 
 
 # Load environment variables
@@ -17,6 +13,7 @@ load_dotenv()
 X_ENDPOINT = "https://api.twitter.com/2/tweets"
 X_BEARER_TOKEN = os.getenv("X_BEARER_TOKEN")
 X_API_KEY = os.getenv("X_CLIENT_ID")
+X_API_KEY_SECRET = os.getenv("X_API_KEY_SECRET")
 X_API_SECRET = os.getenv("X_CLIENT_SECRET")
 X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
 X_ACCESS_TOKEN_SECRET = os.getenv("X_ACCESS_TOKEN_SECRET")
@@ -24,7 +21,6 @@ X_ACCESS_TOKEN_SECRET = os.getenv("X_ACCESS_TOKEN_SECRET")
 
 def test_main():
     """Test function for AI-agent."""
-    # TODO: Add tests for AI-agent
     print("Testing AI-agent...")
     client = OpenAIClient()
     response = client.chat("Hello!")
@@ -33,21 +29,5 @@ def test_main():
     print("---\n Done testing AI-agent. \n---")
 
 
-def test_auth():
-    """Test function for authentication."""
-    url = "https://api.twitter.com/2/users/me"
-    auth = OAuth1(
-        X_API_KEY,
-        client_secret=X_API_SECRET,
-        resource_owner_key=X_ACCESS_TOKEN,
-        resource_owner_secret=X_ACCESS_TOKEN_SECRET
-    )
-    response = requests.get(url, auth=auth)
-    print(f"Test Status: {response.status_code}")
-    print(f"Test Response: {response.text}")
-
-
 if __name__ == "__main__":
-    test_content_creator()
-    # test_auth() BUG: Fix this
     test_main()
